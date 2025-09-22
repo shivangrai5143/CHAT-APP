059fcb7b912071595bfa04ae08947cc7424f98fa
shivangrai5143 <raishivang69@gmail.com>
2025-09-23 00:01:45 +0530

    addiong css property

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -9,6 +9,25 @@ const ChatBox = () => {
       <p>Anuj Yadav<img className='dot' src={assets.green_dot} alt=""/></p>
       <img src={assets.help_icon} className='Help' alt=""/>
       </div>
+
+     <div className="chat-msg">
+      <div className="s-msg">
+        <p className="msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div>
+          <img src = {assets.profile_img}alt=""/>
+          <p>2:30 PM</p>
+        </div>
+      </div>
+       <div className="r-msg">
+        <p className="msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div>
+          <img src = {assets.profile_img}alt=""/>
+          <p>2:30 PM</p>
+        </div>
+      </div>
+    </div>
+
+
       <div className="chat-input">
         <input type="text" placeholder='Send a message'/>
         <input type="file" id="image" accept= 'image/png , image/jpeg' hidden/>
