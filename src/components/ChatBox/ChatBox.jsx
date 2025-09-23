39e6a2ceaec8bce646f21c69b0846466ce5825f4
shivangrai5143 <raishivang69@gmail.com>
2025-09-23 22:29:49 +0530

    creating chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -18,6 +18,15 @@ const ChatBox = () => {
           <p>2:30 PM</p>
         </div>
       </div>
+      
+       <div className="s-msg">
+        <img className='msg-img' src={assets.pic1} alt=""/>
+        <div>
+          <img src = {assets.profile_img}alt=""/>
+          <p>2:30 PM</p>
+        </div>
+      </div>
+
        <div className="r-msg">
         <p className="msg">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         <div>
