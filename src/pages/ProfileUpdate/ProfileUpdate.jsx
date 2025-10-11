c5f8f9d628d21e33a2e0f6b81f3cff99b0054068
shivangrai5143 <raishivang69@gmail.com>
2025-10-11 20:36:09 +0530

    chat-app

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./ProfileUpdate.css"
-function ProfileUpdate() {
+import assets from '../../assets/assets'
+const ProfileUpdate =() =>{
   return (
-    <div>
-      
+    <div className='profile'>
+      <div className="profile-container">
+        <form >
+          <h3>Profile Details</h3>
+          <label htmlFor='avatar'>
+            <input type='file' id='avatar' accept='.png,.jpeg,.jpg' hidden/>
+           <img src ={assets.avatar_icon} alt=""/>
+           upload profile image
+          </label>
+          <input type='text' placeholder='Your name' required/>
+          <textarea placeholder='Write profile bio!' required></textarea>
+          <button type='submit'>Save</button>
+        </form>
+        <img src={assets.logo_icon} alt=''/>
+      </div>
     </div>
   )
 }
