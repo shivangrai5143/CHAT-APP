ef3df8cc8ffabe822597edfa58db82e649d41260
shivangrai5143 <raishivang69@gmail.com>
2025-11-07 23:59:14 +0530

    chat-app

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,18 +8,25 @@ const Login = () => {
   const [currState, setCurrState] = useState("Sign up");
   const [userName,setUserName] = useState("");
   const[email,setEmail]=useState("");
-   const[password,setPassword]= useState ("");
+  const[password,setPassword]= useState ("");
 
-   
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if(currState === "Sign up"){
+      signup(userName,email,password);
+    }
+
+  }
+
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className='logo'/>
-       <form  className="login-form">
+       <form onSubmit={onSubmitHandler}  className="login-form">
         <h2>{currState}</h2>
         {currState === "Sign up"?<input onChange={(e)=>setUserName(e.target.value)} value={userName} type="text" placeholder='Username' className='form-input' required />:null}
         <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Email address' className='form-input' required />
         <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder='Password'  className='form-input' required />
-        <button type="submit">{currState === "Sign Up"?"Create account":"Login now"}</button>
+        <button type="submit">{currState === "Sign up"?"Create account":"Login now"}</button>
         <div className="login-term">
           <input type ="checkbox"/>
           <p>Agree to the terms of use & privacy policy</p>
