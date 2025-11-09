76627b82b7231f611165c6263de494cfebfb05e6
shivangrai5143 <raishivang69@gmail.com>
2025-11-09 20:36:20 +0530

    chat-app

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import "./login.css"
 import assets from '../../assets/assets'
-import {signup} from '../../config/firebase'
+import {signup, login} from '../../config/firebase'
 
 const Login = () => {
   
@@ -15,7 +15,9 @@ const Login = () => {
     if(currState === "Sign up"){
       signup(userName,email,password);
     }
-
+    else{
+       login(email,password);
+    }
   }
 
   return (
