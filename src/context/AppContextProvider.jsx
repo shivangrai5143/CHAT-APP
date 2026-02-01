bdab5831987ffc8f57243ed86a51f0c547586122
shivangrai5143 <raishivang69@gmail.com>
2026-02-01 15:21:00 +0530

    chat-app

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react"; // 1. Added createContext
+import { createContext, useState } from "react"; 
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { auth, db } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
