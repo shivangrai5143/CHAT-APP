d133b5c8c39931d241788c14bac32bd7fb57ca34
shivangrai5143 <raishivang69@gmail.com>
2026-02-01 16:03:54 +0530

    chat-app

---
+++ src/lib/cloudinary.js
@@ -0,0 +1,33 @@
+export const uploadImageToCloudinary = async (file, onProgress) => {
+  const data = new FormData();
+  data.append("file", file);
+  data.append("upload_preset", "Chat_images"); 
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(
+      "POST",
+      "https://api.cloudinary.com/v1_1/du3hiflqj/image/upload"
+    );
+
+    xhr.upload.onprogress = (event) => {
+      if (event.lengthComputable && onProgress) {
+        const percent = Math.round((event.loaded / event.total) * 100);
+        onProgress(percent);
+      }
+    };
+
+    xhr.onload = () => {
+      const response = JSON.parse(xhr.responseText);
+      if (response.secure_url) {
+        resolve(response.secure_url); // ✅ image URL
+      } else {
+        reject(response);
+      }
+    };
+
+    xhr.onerror = () => reject("Upload error");
+
+    xhr.send(data);
+  });
+};
