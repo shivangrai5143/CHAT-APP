1cca590c29497cbe82b5d8c5d66bb61d6e9346e6
shivangrai5143 <raishivang69@gmail.com>
2026-03-21 13:20:14 +0530

    chat-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Login from "./pages/login/login";
+import Login from "./pages/Login/Login";
 import Chat from "./pages/Chat/Chat";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
 import { ToastContainer } from "react-toastify";
